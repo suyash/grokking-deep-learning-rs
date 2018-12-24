09cf1dbee811dd03eeae5e9f47f3d25c3df56ecd
Suyash <suyash93@protonmail.com>
2018-12-24 13:58:45 +0530

    add Chapter 3

---
+++ examples/chapter3.rs
@@ -0,0 +1,123 @@
+//! Grokking Deep Learning - Chapter 3
+//!
+//! https://github.com/iamtrask/Grokking-Deep-Learning/blob/master/Chapter3%20-%20%20Forward%20Propagation%20-%20Intro%20to%20Neural%20Prediction.ipynb
+
+extern crate grokking_deep_learning_rs;
+
+use grokking_deep_learning_rs::{
+    dot, elementwise_scalar_multiplication, vector_matrix_multiplication, Matrix, Vector,
+};
+
+fn main() {
+    // different sections of the chapter in order.
+    what_is_a_neural_network();
+    making_a_prediction_with_multiple_inputs();
+    making_a_prediction_with_multiple_outputs();
+    predicting_with_multiple_inputs_and_outputs();
+    predicting_on_predictions();
+}
+
+/// A Simple Neural Network making a prediction
+///
+/// What is a neural network?
+
+fn what_is_a_neural_network() {
+    let number_of_toes = vec![8.5, 9.5, 10.0, 9.0];
+
+    let input = number_of_toes[0];
+    let weight = 0.1;
+
+    let prediction = neural_network_1(input, weight);
+    println!("prediction: {}", prediction);
+}
+
+fn neural_network_1(input: f64, weight: f64) -> f64 {
+    let prediction = input * weight;
+    prediction
+}
+
+/// Making a prediction with multiple inputs
+
+fn making_a_prediction_with_multiple_inputs() {
+    let toes = vec![8.5, 9.5, 9.9, 9.0];
+    let wlrec = vec![0.65, 0.8, 0.8, 0.9];
+    let nfans = vec![1.2, 1.3, 0.5, 1.0];
+
+    let input = vec![toes[0], wlrec[0], nfans[0]];
+    let weights = vec![0.1, 0.2, 0.0];
+
+    let pred = neural_network_2(input, weights);
+    println!("prediction: {}", pred);
+}
+
+fn neural_network_2(input: Vec<f64>, weights: Vec<f64>) -> f64 {
+    dot(input, &weights)
+}
+
+/// Making a prediction with multiple outputs
+
+fn making_a_prediction_with_multiple_outputs() {
+    let wlrec = vec![0.65, 0.8, 0.8, 0.9];
+
+    let input = wlrec[0];
+    let weights = vec![0.3, 0.2, 0.9];
+
+    let pred = neural_network_3(input, weights);
+    println!("predictions: {:?}", pred);
+}
+
+fn neural_network_3(input: f64, weights: Vec<f64>) -> Vec<f64> {
+    elementwise_scalar_multiplication(weights, input)
+}
+
+/// Predicting with multiple inputs and outputs
+
+fn predicting_with_multiple_inputs_and_outputs() {
+    let toes = vec![8.5, 9.5, 9.9, 9.0];
+    let wlrec = vec![0.65, 0.8, 0.8, 0.9];
+    let nfans = vec![1.2, 1.3, 0.5, 1.0];
+
+    let input = vec![toes[0], wlrec[0], nfans[0]];
+    let weights = vec![
+        vec![0.1, 0.1, -0.3],
+        vec![0.1, 0.2, 0.0],
+        vec![0.0, 1.3, 0.1],
+    ];
+
+    let pred = neural_network_4(input, weights);
+    println!("predictions: {:?}", pred);
+}
+
+fn neural_network_4(input: Vector, weights: Matrix) -> Vector {
+    vector_matrix_multiplication(input, weights)
+}
+
+/// Predicting on Predictions
+
+fn predicting_on_predictions() {
+    let toes = vec![8.5, 9.5, 9.9, 9.0];
+    let wlrec = vec![0.65, 0.8, 0.8, 0.9];
+    let nfans = vec![1.2, 1.3, 0.5, 1.0];
+
+    let input = vec![toes[0], wlrec[0], nfans[0]];
+    let input_weights = vec![
+        vec![0.1, 0.2, -0.1],
+        vec![-0.1, 0.1, 0.9],
+        vec![0.1, 0.4, 0.1],
+    ];
+    let hidden1_weights = vec![
+        vec![0.3, 1.1, -0.3],
+        vec![0.1, 0.2, 0.0],
+        vec![0.0, 1.3, 0.1],
+    ];
+
+    let pred = neural_network_5(input, input_weights, hidden1_weights);
+    println!("predictions: {:?}", pred);
+}
+
+fn neural_network_5(input: Vector, input_weights: Matrix, hidden1_weights: Matrix) -> Vector {
+    vector_matrix_multiplication(
+        vector_matrix_multiplication(input, input_weights),
+        hidden1_weights,
+    )
+}
---
+++ src/lib.rs
@@ -0,0 +1,109 @@
+pub type Vector = Vec<f64>;
+pub type Matrix = Vec<Vec<f64>>;
+
+pub fn elementwise_multiplication(vec_a: Vector, vec_b: &[f64]) -> Vector {
+    vec_a
+        .into_iter()
+        .zip(vec_b.iter())
+        .map(|(a, b)| a * b)
+        .collect()
+}
+
+pub fn vector_sum(vec: Vector) -> f64 {
+    vec.into_iter().sum()
+}
+
+pub fn dot(vec_a: Vector, vec_b: &[f64]) -> f64 {
+    elementwise_multiplication(vec_a, vec_b).into_iter().sum()
+}
+
+pub fn elementwise_scalar_multiplication(vec: Vector, n: f64) -> Vector {
+    vec.into_iter().map(|x| x * n).collect()
+}
+
+pub fn elementwise_addition(vec_a: Vector, vec_b: Vector) -> Vector {
+    vec_a
+        .into_iter()
+        .zip(vec_b.iter())
+        .map(|(a, b)| a + b)
+        .collect()
+}
+
+pub fn vector_average(vec: Vector) -> f64 {
+    let len = vec.len() as f64;
+    vec.into_iter().sum::<f64>() / len
+}
+
+pub fn vector_matrix_multiplication(vec: Vector, mat: Matrix) -> Vector {
+    mat.into_iter().map(|w| dot(w, &vec)).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_elementwise_multiplication() {
+        assert_eq!(
+            vec![6.0, 14.0, 24.0, 36.0, 0.0],
+            elementwise_multiplication(
+                vec![1.0, 2.0, 3.0, 4.0, 5.0],
+                &vec![6.0, 7.0, 8.0, 9.0, 0.0],
+            ),
+        );
+    }
+
+    #[test]
+    fn test_vector_sum() {
+        assert_eq!(15.0, vector_sum(vec![1.0, 2.0, 3.0, 4.0, 5.0]));
+    }
+
+    #[test]
+    fn test_elementwise_addition() {
+        assert_eq!(
+            vec![7.0, 9.0, 11.0, 13.0, 5.0],
+            elementwise_addition(vec![1.0, 2.0, 3.0, 4.0, 5.0], vec![6.0, 7.0, 8.0, 9.0, 0.0],),
+        )
+    }
+
+    #[test]
+    fn test_vector_average() {
+        assert_eq!(3.0, vector_average(vec![1.0, 2.0, 3.0, 4.0, 5.0]));
+    }
+
+    #[test]
+    fn test_dot() {
+        assert_eq!(
+            80.0,
+            dot(
+                vec![1.0, 2.0, 3.0, 4.0, 5.0],
+                &vec![6.0, 7.0, 8.0, 9.0, 0.0],
+            ),
+        );
+    }
+
+    #[test]
+    fn test_elementwise_scalar_multiplication() {
+        assert_eq!(
+            vec![2.0, 4.0, 6.0, 8.0, 10.0],
+            elementwise_scalar_multiplication(vec![1.0, 2.0, 3.0, 4.0, 5.0], 2.0,)
+        )
+    }
+
+    #[test]
+    fn test_vector_matrix_multiplication() {
+        assert_eq!(
+            vec![55.0, 45.0, 40.0, 40.0, 35.0],
+            vector_matrix_multiplication(
+                vec![1.0, 2.0, 3.0, 4.0, 5.0],
+                vec![
+                    vec![1.0, 2.0, 3.0, 4.0, 5.0],
+                    vec![2.0, 3.0, 4.0, 5.0, 1.0],
+                    vec![3.0, 4.0, 5.0, 1.0, 2.0],
+                    vec![4.0, 5.0, 1.0, 2.0, 3.0],
+                    vec![5.0, 4.0, 3.0, 2.0, 1.0],
+                ],
+            ),
+        );
+    }
+}
